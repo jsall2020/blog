ae88d677d7e7d9ebf58a0cc97e1b707f11e892fd
Jennifer Sall <jennifer.sall@gmail.com>
2020-06-30 14:33:17 -0400

    strong parameters added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,16 @@ class ArticlesController < ApplicationController
 		def new
 		end
 	
-		def create
-			@article = Article.new(params[:article])
+
+	def create
+  	@article = Article.new(article_params)
  
-  		@article.save
-  		redirect_to @article
-		end
+  	@article.save
+  	redirect_to @article
+	end
+ 
+	private
+  	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
 end
